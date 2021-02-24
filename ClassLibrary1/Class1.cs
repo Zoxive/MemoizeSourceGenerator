0d9405f729b619e384fa8c124374472c832f0fcf
Kyle Wascher <Kyle.Wascher@infor.com>
2021-02-24 15:16:09 -0500

    add example class lib proj

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Memoized;
+using MemoizeSourceGenerator.Attribute;
+
+namespace ClassLibrary1
+{
+    public static class Class1
+    {
+        public static void AddServices(IServiceCollection services)
+        {
+            services.AddMemoizedSingleton<IExample, Example>();
+        }
+    }
+
+    public class Example : IExample
+    {
+    }
+
+    [CreateMemoizedImplementation]
+    public interface IExample
+    {
+    }
+}
